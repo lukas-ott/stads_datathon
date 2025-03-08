d14f56c3af079f85270b14feac97a1a14f1d3dfa
lukas-ott <lukasott1204@gmail.com>
2025-03-08 08:18:58 +0100

    add boiler plate llm code

--- backend/llm_explanation.py
+++ backend/llm_explanation.py
@@ -1,20 +1,24 @@
-from openai import OpenAI
+import openai
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+load_dotenv(dotenv_path='.env.local')
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a haiku about recursion in programming."
-        }
-    ]
-)
+api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = api_key
 
-print(completion.choices[0].message)
+
+
+def get_explanation(question, answer):
+    completion = openai.ChatCompletion.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": question
+            }
+        ]
+    )
+
+    return completion.choices[0].message['content']
